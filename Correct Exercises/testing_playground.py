14e684fcd9536f0740795225be70ec77895274c4
LaurieGale10 <60512477+LaurieGale10@users.noreply.github.com>
2023-10-25 10:34:15 +0100

    Revised exercise collection

--- Correct Exercises/testing_playground.py
+++
@@ -1,7 +0,0 @@
-import unittest
-#Step 1: Get program in JSON form
-#Step 2: Do initial checks e.g. does it differ from original?
-#Step 3 (if program doesn't require output): Run it; check its output
-program = 'width = 50\ndepth = 25\nheight = depth\nvolume = height * width * depth\nprint("The volume is,volume")'
-exec(program)
-
